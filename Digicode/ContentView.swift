3995e321b093ac51d819cc0d0fc94619240dea58
KevinJeanMarie <kjeanmarie01@gmail.com>
2023-03-29 16:34:07 +0200

    work in progress

--- Digicode/ContentView.swift
+++ Digicode/ContentView.swift
@@ -6,18 +6,42 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseAuthCombineSwift
 
 struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                .foregroundColor(.accentColor)
-                //Spacer()
-            }
-            Text("Hello, world!")
-        }
+            Image(systemName: "person.crop.circle.fill.badge.plus")
+                .imageScale(.large)
+                .font(.system(size: 50))                .foregroundColor(.accentColor)
+            Text("Please login")
+            
+                TextField("Login", text: $email)
+                .font(Font.system(size:18))
+                .padding(8)
+                .background(RoundedRectangle(cornerRadius:50).fill(Color.blue))
+                .padding(5)
+                .offset(x:8, y: 10)
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+            
+                SecureField("Password", text : $password)
+                .font(Font.system(size:18))
+                .padding(8)
+                .background(RoundedRectangle(cornerRadius:50).fill(Color.blue))
+                .padding(5)
+                .offset(x:8, y: 10)
+            
+            Button("Login", action: {
+                print("Login")
+            })
+            .font(Font.system(size:25))
+            .offset(y: 30)            }
             .padding()
     }
 }
